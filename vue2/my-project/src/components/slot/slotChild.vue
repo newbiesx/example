acfe1c9f0040f612d5086f9c1c2ff4530b45f137
lanlan <songxiangit@163.com>
2023-03-22 15:27:08 +0800

    '插槽'

---
+++ vue2/my-project/src/components/slot/slotChild.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div>
+        匿名插槽
+        <slot></slot>
+        <div>
+          <slot name="content"></slot>
+        </div>
+        <h3>作用域插槽</h3>
+        <span>
+          <slot v-bind:user="user">
+            {{ user.lastName }}
+          </slot>
+        </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      user:{
+        firstName:"诸葛",
+        lastName:"亮"
+      }
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style scoped>
+.container{}
+</style>
---
+++ vue2/my-project/src/views/slot/slot.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <slot-child>
+        <!-- <div>父组件匿名插槽内容</div> -->
+        <template v-slot:content>
+          父组件的具名插槽内容
+        </template>
+        <template v-slot="{user}">{{ user&&user.firstName }}</template>
+    </slot-child>
+  </div>
+</template>
+
+<script>
+import slotChild from '../../components/slot/slotChild.vue';
+export default {
+  components: {slotChild},
+  props: {},
+  data() {
+    return {
+      user:null
+    };
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created() {},
+  mounted() {}
+};
+</script>
+<style scoped>
+.container{}
+</style>
